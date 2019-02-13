9be3560deeeec3a30d0552b5d8891147b9b68eca
HERNAN PEREZ INESTROSA SANCHEZ <66634544@iecisa.corp>
2019-02-13 14:22:20 +0100

    GPA-1764

--- gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/dto/serveis/portal/consulta/DadesIdentificacioRDTO.java
+++ gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/dto/serveis/portal/consulta/DadesIdentificacioRDTO.java
@@ -8,21 +8,22 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-@ApiModel(value="DadesIdentificacio")
+@ApiModel(value = "DadesIdentificacio")
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-    "informacioCiutada",
-    "nivellAutenticacio",
-    "terminisDuracio"
-})
+@JsonPropertyOrder({ "informacioCiutada", "nivellAutenticacio", "endpointSignatura", "endpointAutenticacio", "terminisDuracio" })
 @Getter
 @Setter
 public class DadesIdentificacioRDTO {
-	
-	@ApiModelProperty(value="Informació complementària per a mostrar al ciutadà")
-    private String informacioCiutada;
-	@ApiModelProperty(value="Nivell d'autenticació requerit")
-    private String nivellAutenticacio;
-    private TerminisDuracioRDTO terminisDuracio;
-    
+
+	@ApiModelProperty(value = "Informació complementària per a mostrar al ciutadà")
+	private String informacioCiutada;
+	@ApiModelProperty(value = "Nivell d'autenticació requerit")
+	private String nivellAutenticacio;
+	@ApiModelProperty(value = "Endpoint de signatura")
+	private String endpointSignatura;
+	@ApiModelProperty(value = "Endpoint d'autenticació")
+	private String endpointAutenticacio;
+	@ApiModelProperty(value = "Terminis de duració")
+	private TerminisDuracioRDTO terminisDuracio;
+
 }
