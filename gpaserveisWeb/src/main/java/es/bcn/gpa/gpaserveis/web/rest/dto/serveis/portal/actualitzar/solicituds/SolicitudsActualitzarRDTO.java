5145ad9488ae2679bc071e8c24216f490f9f2c4c
HERNAN PEREZ INESTROSA SANCHEZ <66634544@iecisa.corp>
2019-03-11 12:03:17 +0100

    Integración OpenText

--- gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/dto/serveis/portal/actualitzar/solicituds/SolicitudsActualitzarRDTO.java
+++ gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/dto/serveis/portal/actualitzar/solicituds/SolicitudsActualitzarRDTO.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import es.bcn.gpa.gpaserveis.web.rest.dto.serveis.portal.PersonesRDTO;
-import es.bcn.gpa.gpaserveis.web.rest.dto.serveis.portal.crear.solicituds.UnitatGestoraSolicitudsCrearRDTO;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -14,13 +13,14 @@ import lombok.Setter;
 
 @ApiModel(value = "SolicitudActualitzar")
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "procediment", "unitatGestora", "sollicitant", "representant", "solicitant", "representant", "documents", "atributs" })
+@JsonPropertyOrder({ "sollicitant", "representant", "dadesOperacio" })
 @Getter
 @Setter
 public class SolicitudsActualitzarRDTO {
 
-	@ApiModelProperty(value = "Unitat Gestora que tramitarà inicialment la sol·licitud", required = false)
-	private UnitatGestoraSolicitudsCrearRDTO unitatGestora;
+	// @ApiModelProperty(value = "Unitat Gestora que tramitarà inicialment la
+	// sol·licitud", required = false)
+	// private UnitatGestoraSolicitudsCrearRDTO unitatGestora;
 	@ApiModelProperty(value = "Persona sol·licitant", required = false)
 	private PersonesRDTO sollicitant;
 	@ApiModelProperty(value = "Persona representant", required = false)
