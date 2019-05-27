e399c4b2f3d4ff3ab4ed388cf84a59af3b7979c4
HERNAN PEREZ INESTROSA SANCHEZ <66634544@iecisa.corp>
2019-05-27 12:25:14 +0200

    Mapper para retorn notificacions

--- gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/controller/utils/mapper/notificacions/retorn/RetornNotificacioRDTOToActualizarNotificacionDocumentRDTOMapper.java
+++ gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/controller/utils/mapper/notificacions/retorn/RetornNotificacioRDTOToActualizarNotificacionDocumentRDTOMapper.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import es.bcn.gpa.gpaserveis.rest.client.api.model.gpadocumentacio.ActualitzarNotificacionDocument;
 import es.bcn.gpa.gpaserveis.web.rest.controller.utils.converter.common.DataHoraToInternalConverter;
 import es.bcn.gpa.gpaserveis.web.rest.controller.utils.converter.common.DataToInternalConverter;
+import es.bcn.gpa.gpaserveis.web.rest.controller.utils.converter.common.InternalToBigDecimalConverter;
 import es.bcn.gpa.gpaserveis.web.rest.dto.serveis.notificacions.estat.RetornNotificacioRDTO;
 
 /**
@@ -14,18 +16,21 @@ import es.bcn.gpa.gpaserveis.web.rest.dto.serveis.notificacions.estat.RetornNoti
  */
 @Component("notificacioRetornNotificacioRDTOToActualizarNotificacionDocumentRDTOMapper")
 public class RetornNotificacioRDTOToActualizarNotificacionDocumentRDTOMapper
-        extends PropertyMap<RetornNotificacioRDTO, /* ActualizarNotificacionDocumentRDTO */ String> {
+        extends PropertyMap<RetornNotificacioRDTO, ActualitzarNotificacionDocument> {
 
 	private DataToInternalConverter dataToInternalConverter;
 	private DataHoraToInternalConverter dataHoraToInternalConverter;
+	private InternalToBigDecimalConverter internalToBigDecimalConverter;
 
 	@Autowired
 	public RetornNotificacioRDTOToActualizarNotificacionDocumentRDTOMapper(
 	        @Qualifier("dataHoraToInternalConverter") DataHoraToInternalConverter dataHoraToInternalConverter,
-	        @Qualifier("dataToInternalConverter") DataToInternalConverter dataToInternalConverter) {
+	        @Qualifier("dataToInternalConverter") DataToInternalConverter dataToInternalConverter,
+	        @Qualifier("internalToBigDecimalConverter") InternalToBigDecimalConverter internalToBigDecimalConverter) {
 
 		this.dataHoraToInternalConverter = dataHoraToInternalConverter;
 		this.dataToInternalConverter = dataToInternalConverter;
+		this.internalToBigDecimalConverter = internalToBigDecimalConverter;
 	}
 
 	/*
@@ -35,14 +40,14 @@ public class RetornNotificacioRDTOToActualizarNotificacionDocumentRDTOMapper
 	 */
 	@Override
 	protected void configure() {
-		// map().setNotificacioId(source.getNotificacioId());
-		// map().setDocEvidenciaElectronic(source.getDocEvidenciaElectronic());
-		// map().setDocEvidenciaPaper(source.getDocEvidenciaPaper());
-		// using(dataToInternalConverter).map(source.getDataAcusamentRebuda()).setDataAcusamentRebuda(null);
-		// using(dataToInternalConverter).map(source.getDataEnviament()).setDataEnviament(null);
-		// using(dataHoraToInternalConverter).map(source.getDataHoraAceptacioRebuig()).setDataHoraAceptacioRebuig(null);
-		// using(dataHoraToInternalConverter).map(source.getDataHoraDiposit()).setDataHoraDiposit(null);
-		// using(dataHoraToInternalConverter).map(source.getDataHoraVisualitzacio()).setDataHoraVisualitzacio(null);
-		// using(dataToInternalConverter).map(source.getDataLimit()).setDataLimit(null);
+		using(internalToBigDecimalConverter).map(source.getNotificacioId()).setNotificacioId(null);
+		map().setDocEvidenciaElectronic(source.getDocEvidenciaElectronic());
+		map().setDocEvidenciaPaper(source.getDocEvidenciaPaper());
+		using(dataToInternalConverter).map(source.getDataAcusamentRebuda()).setDataAcusamentRebuda(null);
+		using(dataToInternalConverter).map(source.getDataEnviament()).setDataEnviament(null);
+		using(dataHoraToInternalConverter).map(source.getDataHoraAceptacioRebuig()).setDataAceptacioRebuig(null);
+		using(dataHoraToInternalConverter).map(source.getDataHoraDiposit()).setDataHoraDiposit(null);
+		using(dataHoraToInternalConverter).map(source.getDataHoraVisualitzacio()).setDataHoraVisualitzacio(null);
+		using(dataToInternalConverter).map(source.getDataLimit()).setDataLimit(null);
 	}
 }
