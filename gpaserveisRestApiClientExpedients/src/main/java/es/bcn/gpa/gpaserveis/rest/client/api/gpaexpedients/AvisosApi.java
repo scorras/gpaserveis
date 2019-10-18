0049aeeab37a98916f8d9074113470c388f287a1
mapinilla <miguelangel_pinilla@iecisa.com>
2019-10-18 12:16:11 +0200

    GPA-2794

--- gpaserveisRestApiClientExpedients/src/main/java/es/bcn/gpa/gpaserveis/rest/client/api/gpaexpedients/AvisosApi.java
+++ gpaserveisRestApiClientExpedients/src/main/java/es/bcn/gpa/gpaserveis/rest/client/api/gpaexpedients/AvisosApi.java
@@ -1,209 +1,108 @@
 package es.bcn.gpa.gpaserveis.rest.client.api.gpaexpedients;
 
-import es.bcn.gpa.gpaserveis.rest.client.invoker.gpaexpedients.ApiClient;
-
-import es.bcn.gpa.gpaserveis.rest.client.api.model.gpaexpedients.AvisCreacioAccio;
 import java.math.BigDecimal;
-import es.bcn.gpa.gpaserveis.rest.client.api.model.gpaexpedients.PageDataOfAvisosRDTO;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2019-10-17T09:06:51.509+02:00")
+import es.bcn.gpa.gpaserveis.rest.client.api.model.gpaexpedients.AvisCreacioAccio;
+import es.bcn.gpa.gpaserveis.rest.client.invoker.gpaexpedients.ApiClient;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2019-08-14T14:00:28.664+02:00")
 @Component("es.bcn.gpa.gpaserveis.rest.client.api.gpaexpedients.AvisosApi")
 public class AvisosApi {
-    private ApiClient apiClient;
+	private ApiClient apiClient;
 
-    public AvisosApi() {
-        this(new ApiClient());
-    }
+	public AvisosApi() {
+		this(new ApiClient());
+	}
 
-    @Autowired
-    public AvisosApi(ApiClient apiClient) {
-        this.apiClient = apiClient;
-    }
+	@Autowired
+	public AvisosApi(ApiClient apiClient) {
+		this.apiClient = apiClient;
+	}
 
-    public ApiClient getApiClient() {
-        return apiClient;
-    }
+	public ApiClient getApiClient() {
+		return apiClient;
+	}
 
-    public void setApiClient(ApiClient apiClient) {
-        this.apiClient = apiClient;
-    }
+	public void setApiClient(ApiClient apiClient) {
+		this.apiClient = apiClient;
+	}
 
-    /**
-     * Crea un nou avís per a l&#39;expedient en funció de l&#39;acció executada
-     * 
-     * <p><b>200</b> - OK
-     * <p><b>201</b> - Created
-     * <p><b>401</b> - Unauthorized
-     * <p><b>403</b> - Forbidden
-     * <p><b>404</b> - Not Found
-     * @param idAccio Identificador de l&#39;acció
-     * @param idExpedient Identificador de l&#39;expedient
-     * @param avisCreacioAccioRDTO Dades del avís
-     * @throws RestClientException if an error occurs while attempting to invoke the API
-     */
-    public void crearAvisAccio(BigDecimal idAccio, BigDecimal idExpedient, AvisCreacioAccio avisCreacioAccioRDTO) throws RestClientException {
-        Object postBody = avisCreacioAccioRDTO;
-        
-        // verify the required parameter 'idAccio' is set
-        if (idAccio == null) {
-            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'idAccio' when calling crearAvisAccio");
-        }
-        
-        // verify the required parameter 'idExpedient' is set
-        if (idExpedient == null) {
-            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'idExpedient' when calling crearAvisAccio");
-        }
-        
-        // create path and map variables
-        final Map<String, Object> uriVariables = new HashMap<String, Object>();
-        uriVariables.put("idAccio", idAccio);
-        uriVariables.put("idExpedient", idExpedient);
-        String path = UriComponentsBuilder.fromPath("/avisos/{idExpedient}/accions/{idAccio}/crear").buildAndExpand(uriVariables).toUriString();
-        
-        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
-        final HttpHeaders headerParams = new HttpHeaders();
-        final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+	/**
+	 * Crea un nou avís per a l&#39;expedient en funció de l&#39;acció executada
+	 * 
+	 * <p>
+	 * <b>200</b> - OK
+	 * <p>
+	 * <b>201</b> - Created
+	 * <p>
+	 * <b>401</b> - Unauthorized
+	 * <p>
+	 * <b>403</b> - Forbidden
+	 * <p>
+	 * <b>404</b> - Not Found
+	 * 
+	 * @param idAccio
+	 *            Identificador de l&#39;acció
+	 * @param idExpedient
+	 *            Identificador de l&#39;expedient
+	 * @param avisCreacioAccioRDTO
+	 *            Dades del avís
+	 * @throws RestClientException
+	 *             if an error occurs while attempting to invoke the API
+	 */
+	public void crearAvisAccio(BigDecimal idAccio, BigDecimal idExpedient, AvisCreacioAccio avisCreacioAccioRDTO)
+	        throws RestClientException {
+		Object postBody = avisCreacioAccioRDTO;
 
-        final String[] accepts = { 
-            "*/*"
-        };
-        final List<MediaType> accept = apiClient.selectHeaderAccept(accepts);
-        final String[] contentTypes = { 
-            "application/json"
-        };
-        final MediaType contentType = apiClient.selectHeaderContentType(contentTypes);
+		// verify the required parameter 'idAccio' is set
+		if (idAccio == null) {
+			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST,
+			        "Missing the required parameter 'idAccio' when calling crearAvisAccio");
+		}
 
-        String[] authNames = new String[] {  };
+		// verify the required parameter 'idExpedient' is set
+		if (idExpedient == null) {
+			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST,
+			        "Missing the required parameter 'idExpedient' when calling crearAvisAccio");
+		}
 
-        ParameterizedTypeReference<Void> returnType = new ParameterizedTypeReference<Void>() {};
-        apiClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, formParams, accept, contentType, authNames, returnType);
-    }
-    /**
-     * Returns all avisos 
-     * 
-     * <p><b>200</b> - OK
-     * <p><b>401</b> - Unauthorized
-     * <p><b>403</b> - Forbidden
-     * <p><b>404</b> - Not Found
-     * @param id id
-     * @param absoluteRowNumberOfFirstRowInCurrentPage The absoluteRowNumberOfFirstRowInCurrentPage parameter
-     * @param absoluteRowNumberOfLastRowInCurrentPage The absoluteRowNumberOfLastRowInCurrentPage parameter
-     * @param currentPageHasNextPage The currentPageHasNextPage parameter
-     * @param currentPageHasPreviousPage The currentPageHasPreviousPage parameter
-     * @param currentPageIsFirstPage The currentPageIsFirstPage parameter
-     * @param currentPageIsLastPage The currentPageIsLastPage parameter
-     * @param currentPageNumber The currentPageNumber parameter
-     * @param dir The dir parameter
-     * @param nextPageNumber The nextPageNumber parameter
-     * @param pageSize The pageSize parameter
-     * @param previousPageNumber The previousPageNumber parameter
-     * @param sort The sort parameter
-     * @param totalElements The totalElements parameter
-     * @param totalPages The totalPages parameter
-     * @return PageDataOfAvisosRDTO
-     * @throws RestClientException if an error occurs while attempting to invoke the API
-     */
-    public PageDataOfAvisosRDTO obtenirAvisosPorIdExpedientUsingGET(BigDecimal id, Integer absoluteRowNumberOfFirstRowInCurrentPage, Integer absoluteRowNumberOfLastRowInCurrentPage, Boolean currentPageHasNextPage, Boolean currentPageHasPreviousPage, Boolean currentPageIsFirstPage, Boolean currentPageIsLastPage, Integer currentPageNumber, String dir, Integer nextPageNumber, Integer pageSize, Integer previousPageNumber, String sort, Long totalElements, Integer totalPages) throws RestClientException {
-        Object postBody = null;
-        
-        // verify the required parameter 'id' is set
-        if (id == null) {
-            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'id' when calling obtenirAvisosPorIdExpedientUsingGET");
-        }
-        
-        // create path and map variables
-        final Map<String, Object> uriVariables = new HashMap<String, Object>();
-        uriVariables.put("id", id);
-        String path = UriComponentsBuilder.fromPath("/avisos/{id}").buildAndExpand(uriVariables).toUriString();
-        
-        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
-        final HttpHeaders headerParams = new HttpHeaders();
-        final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
-        
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "absoluteRowNumberOfFirstRowInCurrentPage", absoluteRowNumberOfFirstRowInCurrentPage));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "absoluteRowNumberOfLastRowInCurrentPage", absoluteRowNumberOfLastRowInCurrentPage));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "currentPageHasNextPage", currentPageHasNextPage));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "currentPageHasPreviousPage", currentPageHasPreviousPage));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "currentPageIsFirstPage", currentPageIsFirstPage));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "currentPageIsLastPage", currentPageIsLastPage));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "currentPageNumber", currentPageNumber));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "dir", dir));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "nextPageNumber", nextPageNumber));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "pageSize", pageSize));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "previousPageNumber", previousPageNumber));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "sort", sort));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "totalElements", totalElements));
-        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "totalPages", totalPages));
+		// create path and map variables
+		final Map<String, Object> uriVariables = new HashMap<String, Object>();
+		uriVariables.put("idAccio", idAccio);
+		uriVariables.put("idExpedient", idExpedient);
+		String path = UriComponentsBuilder.fromPath("/avisos/{idExpedient}/accions/{idAccio}/crear").buildAndExpand(uriVariables)
+		        .toUriString();
 
-        final String[] accepts = { 
-            "*/*"
-        };
-        final List<MediaType> accept = apiClient.selectHeaderAccept(accepts);
-        final String[] contentTypes = { };
-        final MediaType contentType = apiClient.selectHeaderContentType(contentTypes);
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
+		final HttpHeaders headerParams = new HttpHeaders();
+		final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
 
-        String[] authNames = new String[] {  };
+		final String[] accepts = { "*/*" };
+		final List<MediaType> accept = apiClient.selectHeaderAccept(accepts);
+		final String[] contentTypes = { "application/json" };
+		final MediaType contentType = apiClient.selectHeaderContentType(contentTypes);
 
-        ParameterizedTypeReference<PageDataOfAvisosRDTO> returnType = new ParameterizedTypeReference<PageDataOfAvisosRDTO>() {};
-        return apiClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, formParams, accept, contentType, authNames, returnType);
-    }
-    /**
-     * Returns criticitat avis 
-     * 
-     * <p><b>200</b> - OK
-     * <p><b>401</b> - Unauthorized
-     * <p><b>403</b> - Forbidden
-     * <p><b>404</b> - Not Found
-     * @param id id
-     * @return BigDecimal
-     * @throws RestClientException if an error occurs while attempting to invoke the API
-     */
-    public BigDecimal obtenirCriticitatAvisPorIdExpedientUsingGET(BigDecimal id) throws RestClientException {
-        Object postBody = null;
-        
-        // verify the required parameter 'id' is set
-        if (id == null) {
-            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'id' when calling obtenirCriticitatAvisPorIdExpedientUsingGET");
-        }
-        
-        // create path and map variables
-        final Map<String, Object> uriVariables = new HashMap<String, Object>();
-        uriVariables.put("id", id);
-        String path = UriComponentsBuilder.fromPath("/avisos/criticitatAvis/{id}").buildAndExpand(uriVariables).toUriString();
-        
-        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
-        final HttpHeaders headerParams = new HttpHeaders();
-        final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+		String[] authNames = new String[] {};
 
-        final String[] accepts = { 
-            "*/*"
-        };
-        final List<MediaType> accept = apiClient.selectHeaderAccept(accepts);
-        final String[] contentTypes = { };
-        final MediaType contentType = apiClient.selectHeaderContentType(contentTypes);
-
-        String[] authNames = new String[] {  };
-
-        ParameterizedTypeReference<BigDecimal> returnType = new ParameterizedTypeReference<BigDecimal>() {};
-        return apiClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, formParams, accept, contentType, authNames, returnType);
-    }
+		ParameterizedTypeReference<Void> returnType = new ParameterizedTypeReference<Void>() {
+		};
+		apiClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, formParams, accept, contentType, authNames,
+		        returnType);
+	}
 }
