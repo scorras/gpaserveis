fc86b78575e692fa3e0f03495233400c33955873
mapinilla <miguelangel_pinilla@iecisa.com>
2019-10-24 17:05:13 +0200

    GPA-2801

--- gpaserveisRestApiClientExpedients/src/main/java/es/bcn/gpa/gpaserveis/rest/client/api/gpaexpedients/ComentarisApi.java
+++ gpaserveisRestApiClientExpedients/src/main/java/es/bcn/gpa/gpaserveis/rest/client/api/gpaexpedients/ComentarisApi.java
@@ -64,23 +64,24 @@ public class ComentarisApi {
 	 *            Identificador de l&#39;expedient
 	 * @param comentariCreacioAccioRDTO
 	 *            Dades del comentari
+	 * @return Integer
 	 * @throws RestClientException
 	 *             if an error occurs while attempting to invoke the API
 	 */
-	public void crearComentariAccio(BigDecimal idAccio, BigDecimal idExpedient, ComentariCreacioAccio comentariCreacioAccioRDTO)
-	        throws RestClientException {
+	public Integer crearComentariAccio(BigDecimal idAccio, BigDecimal idExpedient, ComentariCreacioAccio comentariCreacioAccioRDTO)
+			throws RestClientException {
 		Object postBody = comentariCreacioAccioRDTO;
 
 		// verify the required parameter 'idAccio' is set
 		if (idAccio == null) {
 			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST,
-			        "Missing the required parameter 'idAccio' when calling crearComentariAccio");
+					"Missing the required parameter 'idAccio' when calling crearComentariAccio");
 		}
 
 		// verify the required parameter 'idExpedient' is set
 		if (idExpedient == null) {
 			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST,
-			        "Missing the required parameter 'idExpedient' when calling crearComentariAccio");
+					"Missing the required parameter 'idExpedient' when calling crearComentariAccio");
 		}
 
 		// create path and map variables
@@ -88,7 +89,62 @@ public class ComentarisApi {
 		uriVariables.put("idAccio", idAccio);
 		uriVariables.put("idExpedient", idExpedient);
 		String path = UriComponentsBuilder.fromPath("/expedients/comentaris/{idExpedient}/accions/{idAccio}/crear")
-		        .buildAndExpand(uriVariables).toUriString();
+				.buildAndExpand(uriVariables).toUriString();
+
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
+		final HttpHeaders headerParams = new HttpHeaders();
+		final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+
+		final String[] accepts = { "*/*" };
+		final List<MediaType> accept = apiClient.selectHeaderAccept(accepts);
+		final String[] contentTypes = { "application/json" };
+		final MediaType contentType = apiClient.selectHeaderContentType(contentTypes);
+
+		String[] authNames = new String[] {};
+
+		ParameterizedTypeReference<Integer> returnType = new ParameterizedTypeReference<Integer>() {
+		};
+		return apiClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, formParams, accept, contentType, authNames,
+				returnType);
+	}
+
+	/**
+	 * Deletes the requested comentari
+	 * 
+	 * <p>
+	 * <b>200</b> - OK
+	 * <p>
+	 * <b>201</b> - Created
+	 * <p>
+	 * <b>401</b> - Unauthorized
+	 * <p>
+	 * <b>403</b> - Forbidden
+	 * <p>
+	 * <b>404</b> - Not Found
+	 * 
+	 * @param idComentari
+	 *            idComentari
+	 * @param idExpedient
+	 *            idExpedient
+	 * @throws RestClientException
+	 *             if an error occurs while attempting to invoke the API
+	 */
+	public void deleteComentari(BigDecimal idComentari, BigDecimal idExpedient) throws RestClientException {
+		Object postBody = idExpedient;
+
+		// verify the required parameter 'idComentari' is set
+		if (idComentari == null) {
+			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST,
+					"Missing the required parameter 'idComentari' when calling deleteComentari");
+		}
+
+		// verify the required parameter 'idExpedient' is set
+		if (idExpedient == null) {
+			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST,
+					"Missing the required parameter 'idExpedient' when calling deleteComentari");
+		}
+
+		String path = UriComponentsBuilder.fromPath("/expedients/comentaris/deleteComentari").build().toUriString();
 
 		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
 		final HttpHeaders headerParams = new HttpHeaders();
@@ -104,6 +160,6 @@ public class ComentarisApi {
 		ParameterizedTypeReference<Void> returnType = new ParameterizedTypeReference<Void>() {
 		};
 		apiClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, formParams, accept, contentType, authNames,
-		        returnType);
+				returnType);
 	}
 }
