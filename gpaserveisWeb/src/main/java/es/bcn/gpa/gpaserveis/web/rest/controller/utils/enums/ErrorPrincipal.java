258a79202ed5ebd497bad155346d59d48692a030
Felipe Lozano Romero <felipe.lozano@iecisa.com>
2019-10-25 14:05:30 +0200

    Commit GPA-2715

--- gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/controller/utils/enums/ErrorPrincipal.java
+++ gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/controller/utils/enums/ErrorPrincipal.java
@@ -73,6 +73,8 @@ public enum ErrorPrincipal {
 	ERROR_DOCUMENTS_UPLOAD_DECLARACIO_RESPONSABLE_NOT_AVAILABLE("4005", "No és possible pujar un fitxer a una declaració responsable"),
 
 	ERROR_DOCUMENTS_EXPEDIENTS_NO_SIGNATS("4006", "Els documents associats a l'expedient no estan signats"),
+	
+	ERROR_DOCUMENTS_EMMAGATZEMAR_FITXER("4007", "No s'ha pogut emmagatzemar el fitxer"),
 
 	/* Integracions */
 	ERROR_ARIADNA("5001", ""),
