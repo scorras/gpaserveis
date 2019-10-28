f85ce24c08d17e8a85e327d5f4aeb088fd2ec2b6
mapinilla <miguelangel_pinilla@iecisa.com>
2019-10-28 11:57:51 +0100

    Se controla los datos de operacion de un expediente para cuando pueden ser nulos.

--- gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/controller/ServeisPortalRestController.java
+++ gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/controller/ServeisPortalRestController.java
@@ -1453,9 +1453,11 @@ public class ServeisPortalRestController extends BaseRestController {
 
 			// Nos quedamos con una copia de los dades de operacio actuales del
 			// expedient para restaurar en caso de error posterior
-			dadesEspecifiquesRDTOListBBDD = new ArrayList<>();
-			for (DadaEspecificaBDTO dadaEspecificaBDTO : dadesExpedientBDTO.getDadesOperacio()) {
-				dadesEspecifiquesRDTOListBBDD.add(dadaEspecificaBDTO.getDadaEspecifica());
+			if (CollectionUtils.isNotEmpty(dadesExpedientBDTO.getDadesOperacio())) {
+				dadesEspecifiquesRDTOListBBDD = new ArrayList<>();
+				for (DadaEspecificaBDTO dadaEspecificaBDTO : dadesExpedientBDTO.getDadesOperacio()) {
+					dadesEspecifiquesRDTOListBBDD.add(dadaEspecificaBDTO.getDadaEspecifica());
+				}
 			}
 
 			// TODO CAMBIAR OBJETOS PARA QUE NO LLEVEN SOLLICITUD, MAS
@@ -1580,7 +1582,7 @@ public class ServeisPortalRestController extends BaseRestController {
 
 		} catch (GPAServeisServiceException e1) {
 			log.error(
-					"sagaRegistrarSolicitudExpedient(DadesExpedientBDTO, RespostaCrearRegistreExpedient, DocsTramitacioRDTO, ExpedientsRegistrarBDTO, DocumentActualizarRegistre)",
+					"sagaEsmenarExpedient(ExpedientEsmenaRDTO, DadesExpedientBDTO, DocsTramitacioRDTO, List<BigDecimal>, Integer, ActualitzarDadesSollicitud, List<DadesEspecifiquesRDTO>)",
 					e1);// $NON-NLS-1$
 		}
 	}
