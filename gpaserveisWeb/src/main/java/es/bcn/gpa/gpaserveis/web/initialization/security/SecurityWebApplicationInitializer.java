129dcca9974fb1f0365f29f37fb1447182c83fdc
HERNAN PEREZ DE INESTROSA SANCHEZ <hernan_perez@iecisa.com>
2019-12-20 15:13:00 +0100

    Securización de la capa

---
+++ gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/initialization/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package es.bcn.gpa.gpaserveis.web.initialization.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * The Class SecurityWebApplicationInitializer.
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
