701e5c13cf4e70c70c9b2cc767130fcf5ab3c4fb
mapinilla <miguelangel_pinilla@iecisa.com>
2020-01-31 12:52:47 +0100

    corrección merge

--- gpaserveisRestApiClientExpedients/src/main/java/es/bcn/gpa/gpaserveis/rest/client/api/model/gpaexpedients/ConvidarTramitarRDTO.java
+++ gpaserveisRestApiClientExpedients/src/main/java/es/bcn/gpa/gpaserveis/rest/client/api/model/gpaexpedients/ConvidarTramitarRDTO.java
@@ -10,12 +10,16 @@
  * Do not edit the class manually.
  */
 
+
 package es.bcn.gpa.gpaserveis.rest.client.api.model.gpaexpedients;
 
 import java.util.Objects;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import es.bcn.gpa.gpaserveis.rest.client.api.model.gpaexpedients.Comentaris;
+import es.bcn.gpa.gpaserveis.rest.client.api.model.gpaexpedients.DropdownItemBDTO;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,91 +29,6 @@ import java.util.List;
  */
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2020-01-29T17:29:47.132+01:00")
 public class ConvidarTramitarRDTO {
-<<<<<<< HEAD
-	@JsonProperty("comentari")
-	private Comentaris comentari = null;
-
-	@JsonProperty("unitatGestoraConvidada")
-	private DropdownItemBDTO unitatGestoraConvidada = null;
-
-	public ConvidarTramitarRDTO comentari(Comentaris comentari) {
-		this.comentari = comentari;
-		return this;
-	}
-
-	/**
-	 * Get comentari
-	 * 
-	 * @return comentari
-	 **/
-	@ApiModelProperty(value = "")
-	public Comentaris getComentari() {
-		return comentari;
-	}
-
-	public void setComentari(Comentaris comentari) {
-		this.comentari = comentari;
-	}
-
-	public ConvidarTramitarRDTO unitatGestoraConvidada(DropdownItemBDTO unitatGestoraConvidada) {
-		this.unitatGestoraConvidada = unitatGestoraConvidada;
-		return this;
-	}
-
-	/**
-	 * Get unitatGestoraConvidada
-	 * 
-	 * @return unitatGestoraConvidada
-	 **/
-	@ApiModelProperty(value = "")
-	public DropdownItemBDTO getUnitatGestoraConvidada() {
-		return unitatGestoraConvidada;
-	}
-
-	public void setUnitatGestoraConvidada(DropdownItemBDTO unitatGestoraConvidada) {
-		this.unitatGestoraConvidada = unitatGestoraConvidada;
-	}
-
-	@Override
-	public boolean equals(java.lang.Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		ConvidarTramitarRDTO convidarTramitarRDTO = (ConvidarTramitarRDTO) o;
-		return Objects.equals(this.comentari, convidarTramitarRDTO.comentari)
-				&& Objects.equals(this.unitatGestoraConvidada, convidarTramitarRDTO.unitatGestoraConvidada);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(comentari, unitatGestoraConvidada);
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("class ConvidarTramitarRDTO {\n");
-
-		sb.append("    comentari: ").append(toIndentedString(comentari)).append("\n");
-		sb.append("    unitatGestoraConvidada: ").append(toIndentedString(unitatGestoraConvidada)).append("\n");
-		sb.append("}");
-		return sb.toString();
-	}
-
-	/**
-	 * Convert the given object to string with each line indented by 4 spaces
-	 * (except the first line).
-	 */
-	private String toIndentedString(java.lang.Object o) {
-		if (o == null) {
-			return "null";
-		}
-		return o.toString().replace("\n", "\n    ");
-	}
-=======
   @JsonProperty("codiUnitatGestoraList")
   private List<DropdownItemBDTO> codiUnitatGestoraList = null;
 
@@ -201,6 +120,6 @@ public class ConvidarTramitarRDTO {
     }
     return o.toString().replace("\n", "\n    ");
   }
->>>>>>> origin/master
 
 }
+
