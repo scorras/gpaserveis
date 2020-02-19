2b143642b3255058afc77520486a4d209d595cb5
HERNAN PEREZ DE INESTROSA SANCHEZ <hernan_perez@iecisa.com>
2020-02-19 17:24:11 +0100

    GPA-3026: Validaciones de Crear Solicitud

--- gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/business/impl/ServeisServiceImpl.java
+++ gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/business/impl/ServeisServiceImpl.java
@@ -71,8 +71,8 @@ import es.bcn.gpa.gpaserveis.business.dto.procediments.ProcedimentsCercaBDTO;
 import es.bcn.gpa.gpaserveis.business.dto.procediments.RespostaDadesOperacioCercaBDTO;
 import es.bcn.gpa.gpaserveis.business.dto.procediments.RespostaDadesOperacioRequeritsCercaBDTO;
 import es.bcn.gpa.gpaserveis.business.dto.procediments.RespostaProcedimentsCercaBDTO;
-import es.bcn.gpa.gpaserveis.business.dto.sollicituds.SollicitudsActualitzarBDTO;
 import es.bcn.gpa.gpaserveis.business.dto.sollicituds.RespostaSollicitudsCercaBDTO;
+import es.bcn.gpa.gpaserveis.business.dto.sollicituds.SollicitudsActualitzarBDTO;
 import es.bcn.gpa.gpaserveis.business.dto.sollicituds.SollicitudsCercaBDTO;
 import es.bcn.gpa.gpaserveis.business.dto.sollicituds.SollicitudsCrearBDTO;
 import es.bcn.gpa.gpaserveis.business.dto.tramits.TramitsOvtCercaBDTO;
@@ -1064,7 +1064,7 @@ public class ServeisServiceImpl implements ServeisService {
 			marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
 
 			JAXBElement<SollicitudConsultaRDTO> jaxbElement = new JAXBElement<SollicitudConsultaRDTO>(new QName(null, "sollicitud"),
-					SollicitudConsultaRDTO.class, sollicitudConsultaRDTO);
+			        SollicitudConsultaRDTO.class, sollicitudConsultaRDTO);
 			stringWriter = new StringWriter();
 			marshaller.marshal(jaxbElement, stringWriter);
 		} catch (JAXBException e) {
@@ -1969,10 +1969,19 @@ public class ServeisServiceImpl implements ServeisService {
 		expedientsService.guardarDadesEspecifiquesSollicitud(idSollicitud);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see es.bcn.gpa.gpaserveis.business.ServeisService#
+	 * actualitzarDadesSollicitudSollicituds(es.bcn.gpa.gpaserveis.business.dto.
+	 * sollicituds.SollicitudsActualitzarBDTO)
+	 */
 	@Override
 	public SollicitudsRDTO actualitzarDadesSollicitudSollicituds(SollicitudsActualitzarBDTO sollicitudsActualitzarBDTO)
-			throws GPAServeisServiceException {
+	        throws GPAServeisServiceException {
 		return sollicitudsService.actualitzarDadesSollicitudSollicituds(sollicitudsActualitzarBDTO);
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * 
