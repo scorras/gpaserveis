8623db849463ea0be4a7a8ed8e7998792a7cadec
mapinilla <miguelangel_pinilla@iecisa.com>
2020-07-14 13:28:32 +0200

    GPA-3302. Pruebas para ver si se mantiene el contexto de seguridad

--- gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/business/config/HystrixConfiguration.java
+++ gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/business/config/HystrixConfiguration.java
@@ -1,6 +1,9 @@
 package es.bcn.gpa.gpaserveis.business.config;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.configuration.AbstractConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +11,14 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import com.netflix.config.ConfigurationManager;
 import com.netflix.hystrix.contrib.javanica.aop.aspectj.HystrixCommandAspect;
+import com.netflix.hystrix.strategy.HystrixPlugins;
+import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+import com.netflix.hystrix.strategy.eventnotifier.HystrixEventNotifier;
+import com.netflix.hystrix.strategy.executionhook.HystrixCommandExecutionHook;
+import com.netflix.hystrix.strategy.metrics.HystrixMetricsPublisher;
+import com.netflix.hystrix.strategy.properties.HystrixPropertiesStrategy;
+
+import es.bcn.gpa.gpaserveis.web.initialization.strategy.SecurityContextConcurrencyStrategy;
 
 @Configuration
 @EnableAspectJAutoProxy
@@ -15,6 +26,12 @@ public class HystrixConfiguration {
 
 	@Value("${execution.isolation.thread.timeoutInMilliseconds}")
 	private String EXECUTION_ISOLATION_THREAD_TIMEOUTINMILLISECONDS;
+
+	// @Autowired(required = false)
+	// private List<HystrixCallableWrapper> wrappers = new ArrayList<>();
+	@Autowired(required = false)
+	// private CustomHystrixConcurrencyStrategy existingConcurrencyStrategy;
+	private HystrixConcurrencyStrategy existingConcurrencyStrategy;
 
 	@Bean
 	public HystrixCommandAspect hystrixAspect() {
@@ -26,9 +43,9 @@ public class HystrixConfiguration {
 		AbstractConfiguration abstractConfiguration = ConfigurationManager.getConfigInstance();
 
 		// Execution
-		abstractConfiguration.setProperty("hystrix.command.default.execution.isolation.strategy", "SEMAPHORE");
+		abstractConfiguration.setProperty("hystrix.command.default.execution.isolation.strategy", "THREAD");
 		abstractConfiguration.setProperty("hystrix.command.default.execution.isolation.thread.timeoutInMilliseconds",
-		        EXECUTION_ISOLATION_THREAD_TIMEOUTINMILLISECONDS);
+				EXECUTION_ISOLATION_THREAD_TIMEOUTINMILLISECONDS);
 		abstractConfiguration.setProperty("hystrix.command.default.execution.timeout.enabled", "true");
 		abstractConfiguration.setProperty("hystrix.command.default.execution.isolation.thread.interruptOnTimeout", "false");
 		abstractConfiguration.setProperty("hystrix.command.default.execution.isolation.thread.interruptOnCancel", "true");
@@ -50,7 +67,30 @@ public class HystrixConfiguration {
 		abstractConfiguration.setProperty("hystrix.threadpool.default.allowMaximumSizeToDivergeFromCoreSize", "true");
 		abstractConfiguration.setProperty("hystrix.threadpool.default.maxQueueSize", "-1");
 
+		abstractConfiguration.setProperty("management.metrics.binders.hystrix.enabled", "false");
+
 		return abstractConfiguration;
+	}
+
+	@PostConstruct
+	public void init() {
+		HystrixPlugins.reset();
+		// Keeps references of existing Hystrix plugins.
+		HystrixEventNotifier eventNotifier = HystrixPlugins.getInstance().getEventNotifier();
+		HystrixMetricsPublisher metricsPublisher = HystrixPlugins.getInstance().getMetricsPublisher();
+		HystrixPropertiesStrategy propertiesStrategy = HystrixPlugins.getInstance().getPropertiesStrategy();
+		HystrixCommandExecutionHook commandExecutionHook = HystrixPlugins.getInstance().getCommandExecutionHook();
+
+		HystrixPlugins.reset();
+
+		// Registers existing plugins excepts the Concurrent Strategy plugin.
+		HystrixPlugins.getInstance().registerConcurrencyStrategy(new SecurityContextConcurrencyStrategy(existingConcurrencyStrategy));
+		HystrixPlugins.getInstance().registerEventNotifier(eventNotifier);
+		HystrixPlugins.getInstance().registerMetricsPublisher(metricsPublisher);
+		HystrixPlugins.getInstance().registerPropertiesStrategy(propertiesStrategy);
+		HystrixPlugins.getInstance().registerCommandExecutionHook(commandExecutionHook);
+		// HystrixPlugins.getInstance().registerCommandExecutionHook(new
+		// SecurityContextRegistratorCommandHook());
 	}
 
 }
---
+++ gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/initialization/strategy/SecurityContextConcurrencyStrategy.java
@@ -0,0 +1,53 @@
+package es.bcn.gpa.gpaserveis.web.initialization.strategy;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
+import org.springframework.stereotype.Component;
+
+import com.netflix.hystrix.HystrixThreadPoolKey;
+import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariable;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariableLifecycle;
+import com.netflix.hystrix.strategy.properties.HystrixProperty;
+
+@Component
+public class SecurityContextConcurrencyStrategy extends HystrixConcurrencyStrategy {
+
+	private HystrixConcurrencyStrategy existingConcurrencyStrategy;
+
+	public SecurityContextConcurrencyStrategy(HystrixConcurrencyStrategy existingConcurrencyStrategy) {
+		this.existingConcurrencyStrategy = existingConcurrencyStrategy;
+	}
+
+	@Override
+	public BlockingQueue<Runnable> getBlockingQueue(int maxQueueSize) {
+		return existingConcurrencyStrategy != null ? existingConcurrencyStrategy.getBlockingQueue(maxQueueSize)
+				: super.getBlockingQueue(maxQueueSize);
+	}
+
+	@Override
+	public <T> HystrixRequestVariable<T> getRequestVariable(HystrixRequestVariableLifecycle<T> rv) {
+		return existingConcurrencyStrategy != null ? existingConcurrencyStrategy.getRequestVariable(rv) : super.getRequestVariable(rv);
+	}
+
+	@Override
+	public ThreadPoolExecutor getThreadPool(HystrixThreadPoolKey threadPoolKey, HystrixProperty<Integer> corePoolSize,
+			HystrixProperty<Integer> maximumPoolSize, HystrixProperty<Integer> keepAliveTime, TimeUnit unit,
+			BlockingQueue<Runnable> workQueue) {
+		return existingConcurrencyStrategy != null
+				? existingConcurrencyStrategy.getThreadPool(threadPoolKey, corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue)
+				: super.getThreadPool(threadPoolKey, corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+	}
+
+	@Override
+	public <T> Callable<T> wrapCallable(Callable<T> callable) {
+		return existingConcurrencyStrategy != null
+				? existingConcurrencyStrategy.wrapCallable(new DelegatingSecurityContextCallable<T>(callable))
+				: super.wrapCallable(new DelegatingSecurityContextCallable<T>(callable));
+	}
+
+}
