7309cf6b99137bbce0d1e7d25ada71b262ab71d3
Fernando Martín Díez <fernando.martin-diez@inetum.world>
2021-05-25 11:42:38 +0200

    GPA-3841,GPA-3842

--- gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/dto/serveis/tramitadors/accions/expedients/tancar/ExpedientTancamentRDTO.java
+++ gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/dto/serveis/tramitadors/accions/expedients/tancar/ExpedientTancamentRDTO.java
@@ -1,5 +1,7 @@
 package es.bcn.gpa.gpaserveis.web.rest.dto.serveis.tramitadors.accions.expedients.tancar;
 
+import java.math.BigDecimal;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
@@ -10,12 +12,14 @@ import lombok.Setter;
 
 @ApiModel(value = "ExpedientTancament")
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "comentari" })
+@JsonPropertyOrder({ "comentari", "tancamentAutomatic" })
 @Getter
 @Setter
 public class ExpedientTancamentRDTO {
 
 	@ApiModelProperty(value = "comentari associat al tancament.")
 	private String comentari;
+	@ApiModelProperty(value = "tancament automatic.")
+	private BigDecimal tancamentAutomatic;
 
 }
