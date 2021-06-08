bb0362dd8b75134a0f04a6f93c8fb4ea31c531a6
Hernán Pérez de Inestrosa Sánchez <hpinestrosa@gmail.com>
2021-06-08 14:37:15 +0200

    Mejora de documentación de API

--- gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/dto/serveis/portal/consulta/DadesIdentificacioRDTO.java
+++ gpaserveisWeb/src/main/java/es/bcn/gpa/gpaserveis/web/rest/dto/serveis/portal/consulta/DadesIdentificacioRDTO.java
@@ -3,6 +3,7 @@ package es.bcn.gpa.gpaserveis.web.rest.dto.serveis.portal.consulta;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import es.bcn.gpa.gpaserveis.web.rest.controller.utils.translator.impl.procediment.NivellAutenticacioApiParamValueTranslator;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -11,14 +12,14 @@ import lombok.Setter;
 @ApiModel(value = "DadesIdentificacio")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({ "informacioCiutada", "nivellAutenticacio", "endpointSignatura", "endpointAutenticacio", "terminisDuracio",
-		"tractamentGdpr" })
+        "tractamentGdpr" })
 @Getter
 @Setter
 public class DadesIdentificacioRDTO {
 
 	@ApiModelProperty(value = "Informació complementària per a mostrar al ciutadà")
 	private String informacioCiutada;
-	@ApiModelProperty(value = "Nivell d'autenticació requerit")
+	@ApiModelProperty(value = "Nivell d'autenticació requerit", allowableValues = NivellAutenticacioApiParamValueTranslator.REQUEST_PARAM_ALLOWABLE_VALUES)
 	private String nivellAutenticacio;
 	@ApiModelProperty(value = "Endpoint de signatura")
 	private String endpointSignatura;
