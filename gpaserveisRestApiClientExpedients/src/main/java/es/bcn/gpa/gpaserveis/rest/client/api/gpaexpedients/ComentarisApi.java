e2b3075b91af7c73edc0f0660157931b16860e29
acarrascosa <alvaro.carrascosa@inetum.com>
2021-09-15 11:55:49 +0200

    Fix v2.9.1

--- gpaserveisRestApiClientExpedients/src/main/java/es/bcn/gpa/gpaserveis/rest/client/api/gpaexpedients/ComentarisApi.java
+++ gpaserveisRestApiClientExpedients/src/main/java/es/bcn/gpa/gpaserveis/rest/client/api/gpaexpedients/ComentarisApi.java
@@ -1,149 +1,170 @@
 package es.bcn.gpa.gpaserveis.rest.client.api.gpaexpedients;
 
-import es.bcn.gpa.gpaserveis.rest.client.invoker.gpaexpedients.ApiClient;
-
 import java.math.BigDecimal;
-import es.bcn.gpa.gpaserveis.rest.client.api.model.gpaexpedients.ComentariCreacioAccio;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import es.bcn.gpa.gpaserveis.rest.client.api.model.gpaexpedients.ComentariCreacioAccio;
+import es.bcn.gpa.gpaserveis.rest.client.invoker.gpaexpedients.ApiClient;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2021-09-15T11:38:28.065+02:00")
 @Component("es.bcn.gpa.gpaserveis.rest.client.api.gpaexpedients.ComentarisApi")
 public class ComentarisApi {
-    private ApiClient apiClient;
+	private ApiClient apiClient;
 
-    public ComentarisApi() {
-        this(new ApiClient());
-    }
+	public ComentarisApi() {
+		this(new ApiClient());
+	}
 
-    @Autowired
-    public ComentarisApi(ApiClient apiClient) {
-        this.apiClient = apiClient;
-    }
+	@Autowired
+	public ComentarisApi(ApiClient apiClient) {
+		this.apiClient = apiClient;
+	}
 
-    public ApiClient getApiClient() {
-        return apiClient;
-    }
+	public ApiClient getApiClient() {
+		return apiClient;
+	}
 
-    public void setApiClient(ApiClient apiClient) {
-        this.apiClient = apiClient;
-    }
+	public void setApiClient(ApiClient apiClient) {
+		this.apiClient = apiClient;
+	}
 
-    /**
-     * Crea un nou comentari per a l&#39;expedient en funció de l&#39;acció executada
-     * 
-     * <p><b>200</b> - OK
-     * <p><b>201</b> - Created
-     * <p><b>401</b> - Unauthorized
-     * <p><b>403</b> - Forbidden
-     * <p><b>404</b> - Not Found
-     * @param idAccio Identificador de l&#39;acció
-     * @param idExpedient Identificador de l&#39;expedient
-     * @param comentariCreacioAccioRDTO Dades del comentari
-     * @return Integer
-     * @throws RestClientException if an error occurs while attempting to invoke the API
-     */
-    public Integer crearComentariAccio(BigDecimal idAccio, BigDecimal idExpedient, ComentariCreacioAccio comentariCreacioAccioRDTO) throws RestClientException {
-        Object postBody = comentariCreacioAccioRDTO;
-        
-        // verify the required parameter 'idAccio' is set
-        if (idAccio == null) {
-            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'idAccio' when calling crearComentariAccio");
-        }
-        
-        // verify the required parameter 'idExpedient' is set
-        if (idExpedient == null) {
-            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'idExpedient' when calling crearComentariAccio");
-        }
-        
-        // create path and map variables
-        final Map<String, Object> uriVariables = new HashMap<String, Object>();
-        uriVariables.put("idAccio", idAccio);
-        uriVariables.put("idExpedient", idExpedient);
-        String path = UriComponentsBuilder.fromPath("/expedients/comentaris/{idExpedient}/accions/{idAccio}/crear").buildAndExpand(uriVariables).toUriString();
-        
-        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
-        final HttpHeaders headerParams = new HttpHeaders();
-        final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+	/**
+	 * Crea un nou comentari per a l&#39;expedient en funció de l&#39;acció
+	 * executada
+	 * 
+	 * <p>
+	 * <b>200</b> - OK
+	 * <p>
+	 * <b>201</b> - Created
+	 * <p>
+	 * <b>401</b> - Unauthorized
+	 * <p>
+	 * <b>403</b> - Forbidden
+	 * <p>
+	 * <b>404</b> - Not Found
+	 * 
+	 * @param idAccio
+	 *            Identificador de l&#39;acció
+	 * @param idExpedient
+	 *            Identificador de l&#39;expedient
+	 * @param comentariCreacioAccioRDTO
+	 *            Dades del comentari
+	 * @return Integer
+	 * @throws RestClientException
+	 *             if an error occurs while attempting to invoke the API
+	 */
+	public Integer crearComentariAccio(BigDecimal idAccio, BigDecimal idExpedient, ComentariCreacioAccio comentariCreacioAccioRDTO)
+			throws RestClientException {
+		Object postBody = comentariCreacioAccioRDTO;
 
-        final String[] accepts = { 
-            "*/*"
-        };
-        final List<MediaType> accept = apiClient.selectHeaderAccept(accepts);
-        final String[] contentTypes = { 
-            "application/json"
-        };
-        final MediaType contentType = apiClient.selectHeaderContentType(contentTypes);
+		// verify the required parameter 'idAccio' is set
+		if (idAccio == null) {
+			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST,
+					"Missing the required parameter 'idAccio' when calling crearComentariAccio");
+		}
 
-        String[] authNames = new String[] {  };
+		// verify the required parameter 'idExpedient' is set
+		if (idExpedient == null) {
+			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST,
+					"Missing the required parameter 'idExpedient' when calling crearComentariAccio");
+		}
 
-        ParameterizedTypeReference<Integer> returnType = new ParameterizedTypeReference<Integer>() {};
-        return apiClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, formParams, accept, contentType, authNames, returnType);
-    }
-    /**
-     * Deletes the requested comentari
-     * 
-     * <p><b>200</b> - OK
-     * <p><b>201</b> - Created
-     * <p><b>401</b> - Unauthorized
-     * <p><b>403</b> - Forbidden
-     * <p><b>404</b> - Not Found
-     * @param idComentari idComentari
-     * @param idExpedient idExpedient
-     * @throws RestClientException if an error occurs while attempting to invoke the API
-     */
-    public void deleteComentari(Integer idComentari, Integer idExpedient) throws RestClientException {
-        Object postBody = null;
-        
-        // verify the required parameter 'idComentari' is set
-        if (idComentari == null) {
-            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'idComentari' when calling deleteComentari");
-        }
-        
-        // verify the required parameter 'idExpedient' is set
-        if (idExpedient == null) {
-            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'idExpedient' when calling deleteComentari");
-        }
-        
-        // create path and map variables
-        final Map<String, Object> uriVariables = new HashMap<String, Object>();
-        uriVariables.put("idComentari", idComentari);
-        uriVariables.put("idExpedient", idExpedient);
-        String path = UriComponentsBuilder.fromPath("/expedients/comentaris/expedient/{idExpedient}/comentari/{idComentari}/delete").buildAndExpand(uriVariables).toUriString();
-        
-        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
-        final HttpHeaders headerParams = new HttpHeaders();
-        final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+		// create path and map variables
+		final Map<String, Object> uriVariables = new HashMap<String, Object>();
+		uriVariables.put("idAccio", idAccio);
+		uriVariables.put("idExpedient", idExpedient);
+		String path = UriComponentsBuilder.fromPath("/expedients/comentaris/{idExpedient}/accions/{idAccio}/crear")
+				.buildAndExpand(uriVariables).toUriString();
 
-        final String[] accepts = { 
-            "*/*"
-        };
-        final List<MediaType> accept = apiClient.selectHeaderAccept(accepts);
-        final String[] contentTypes = { 
-            "application/json"
-        };
-        final MediaType contentType = apiClient.selectHeaderContentType(contentTypes);
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
+		final HttpHeaders headerParams = new HttpHeaders();
+		final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
 
-        String[] authNames = new String[] {  };
+		final String[] accepts = { "*/*" };
+		final List<MediaType> accept = apiClient.selectHeaderAccept(accepts);
+		final String[] contentTypes = { "application/json" };
+		final MediaType contentType = apiClient.selectHeaderContentType(contentTypes);
 
-        ParameterizedTypeReference<Void> returnType = new ParameterizedTypeReference<Void>() {};
-        apiClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, formParams, accept, contentType, authNames, returnType);
-    }
+		String[] authNames = new String[] {};
+
+		ParameterizedTypeReference<Integer> returnType = new ParameterizedTypeReference<Integer>() {
+		};
+		return apiClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, formParams, accept, contentType, authNames,
+				returnType);
+	}
+
+	/**
+	 * Deletes the requested comentari
+	 * 
+	 * <p>
+	 * <b>200</b> - OK
+	 * <p>
+	 * <b>201</b> - Created
+	 * <p>
+	 * <b>401</b> - Unauthorized
+	 * <p>
+	 * <b>403</b> - Forbidden
+	 * <p>
+	 * <b>404</b> - Not Found
+	 * 
+	 * @param idComentari
+	 *            idComentari
+	 * @param idExpedient
+	 *            idExpedient
+	 * @throws RestClientException
+	 *             if an error occurs while attempting to invoke the API
+	 */
+	public void deleteComentari(BigDecimal idComentari, BigDecimal idExpedient) throws RestClientException {
+		Object postBody = null;
+
+		// verify the required parameter 'idComentari' is set
+		if (idComentari == null) {
+			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST,
+					"Missing the required parameter 'idComentari' when calling deleteComentari");
+		}
+
+		// verify the required parameter 'idExpedient' is set
+		if (idExpedient == null) {
+			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST,
+					"Missing the required parameter 'idExpedient' when calling deleteComentari");
+		}
+
+		// create path and map variables
+		final Map<String, Object> uriVariables = new HashMap<String, Object>();
+		uriVariables.put("idComentari", idComentari);
+		uriVariables.put("idExpedient", idExpedient);
+		String path = UriComponentsBuilder.fromPath("/expedients/comentaris/expedient/{idExpedient}/comentari/{idComentari}/delete")
+				.buildAndExpand(uriVariables).toUriString();
+
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
+		final HttpHeaders headerParams = new HttpHeaders();
+		final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+
+		final String[] accepts = { "*/*" };
+		final List<MediaType> accept = apiClient.selectHeaderAccept(accepts);
+		final String[] contentTypes = { "application/json" };
+		final MediaType contentType = apiClient.selectHeaderContentType(contentTypes);
+
+		String[] authNames = new String[] {};
+
+		ParameterizedTypeReference<Void> returnType = new ParameterizedTypeReference<Void>() {
+		};
+		apiClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, formParams, accept, contentType, authNames,
+				returnType);
+	}
 }
